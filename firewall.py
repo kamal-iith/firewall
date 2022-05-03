9db36578f83f600fd7325319b6be071a5b907bb2
kamal-iith <cs21mtech16001@iith.ac.in>
2022-05-03 22:13:07 +0530

    Finished

--- firewall.py
+++ firewall.py
@@ -1,13 +1,10 @@
-import socket
-import click
-import matplotlib.pyplot as plt
 from struct import *
-import struct
-import select
-import time
-import json, os
-import binascii
+import socket, struct, time, os, binascii
+import click, select, json
+import matplotlib.pyplot as plt
 from getkey import getkey
+
+MEMORY_BUFFER = 60000
 
 
 class bcol:
@@ -37,7 +34,7 @@ class SimpleFirewall:
         self.internal_host_mac = "52:54:00:f7:69:35"
         self.external_host_mac = "52:54:00:d6:10:87"
 
-        self.rules = {"BLOCKED_IP_LIST": ["142.250.182.46"]}
+        self.rules = {"BLOCKED_IP_LIST": ["142.250.182.14"]}
 
     def get_ip(self, addr):
         return ".".join(map(str, addr))
@@ -73,7 +70,6 @@ class SimpleFirewall:
                 dest_mac = binascii.unhexlify(self.internal_host_mac.replace(":", ""))
                 new_data = struct.pack("! 6s 6s H", dest_mac, src_mac, type_mac) + raw_data[14:]
                 self.internal_socket.sendall(new_data)
-
         elif eth[1] == self.internal_host_mac:
             allow = True
             dest_mac, src_mac, type_mac = struct.unpack("! 6s 6s H", raw_data[:14])
@@ -81,33 +77,41 @@ class SimpleFirewall:
             new_data = struct.pack("! 6s 6s H", dest_mac, src_mac, type_mac) + raw_data[14:]
             self.external_socket.sendall(new_data)
         else:
-            allow = False
+            allow = True
 
-        return allow, ip[4]
+        return allow, ip[4], ip[5]
 
     def run(self):
         while True:
             all_socks = [self.internal_socket, self.external_socket]
-            ready_socks, _, _ = select.select(all_socks, [], [])
+            interface_sockets, _, _ = select.select(all_socks, [], [])
 
-            for soc in ready_socks:
-                raw_data, _ = soc.recvfrom(65565)
-                ret, ip = self.parse_rules(raw_data)
+            for soc in interface_sockets:
+                raw_data, _ = soc.recvfrom(MEMORY_BUFFER)
+                ret, sip, dip = self.parse_rules(raw_data)
                 if ret:
-                    print(f"{ip}: Allowed")
+                    print(f"Src IP: {sip}\t        Dst IP: {dip}\t\t          {bcol.OKGREEN}Allowed{bcol.ENDC}")
                 else:
-                    print(f"{ip}: Dropped")
+                    print(f"Src IP: {sip}\t        Dst IP: {dip}\t\t          {bcol.FAIL}Dropped{bcol.ENDC}")
 
 
 class Firewall:
-    def __init__(self, interface1, interface2, dos_threshold=0):
+    def __init__(self, interface1, interface2, dos_threshold: int = 100, mapper_file: str = "mapper.json"):
+        """Advanced Firewall Class
+
+        Args:
+            interface1 (Socket): Interface that is connected in to one subnet
+            interface2 (Socket): Interface that is connected to next subnet
+            dos_threshold (int, optional): Number of packets before firewall detects DDoS. Defaults to 0.
+            mapper_file (str, optional): File that accumulates all the informations from raw sockets. Defaults to "mapper.json".
+        """
 
         self.internal_socket, self.external_socket = self.initialize_socket(interface1, interface2)
 
         self.rules_set = {}
-        self.mapping_dict = self.load_mapper("mapper.json")
+        self.mapping_dict = self.load_mapper(mapper_file)
         self.packet = ""
-        self.times = []
+        self.time_stamp = []
 
         self.dos_threshold = dos_threshold
         self.sources_ipv4 = {}
@@ -116,35 +120,67 @@ class Firewall:
         self.plot_allow, self.plot_discard = [], []
         self.allowed, self.discarded = 0, 0
 
-    def load_mapper(self, mapper_file):
+    def load_mapper(self, mapper_file: str):
+        """File to load the elements to capture from the raw sockets
+
+        Args:
+            mapper_file (str, optional): File that accumulates all the informations from raw sockets. Defaults to "mapper.json".
+
+        Returns:
+            _type_: dictionary
+        """
         with open(mapper_file, "r") as handler:
             mapper = json.load(handler)
         return mapper
 
     def initialize_socket(self, interface1, interface2):
+        """Function to initialize the sockets
+
+        Args:
+            interface1 (Socket): First Network
+            interface2 (Socket): Next Network
+
+        Returns:
+            socket, socket: Binded Sockets for both the networks
+        """
+
         internal_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x003))
         external_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+
         internal_socket.bind((interface1, 0))
         external_socket.bind((interface2, 0))
 
         return internal_socket, external_socket
 
-    def parse_L2(self, raw_data, byte_len=14):
+    def parse_L2(self, raw_data: str, byte_len: int = 14):
+        """Parsing the information of the L2 Layers
+
+        Args:
+            raw_data (str): Taking the raw data from the socket
+            byte_len (int, optional): Number of bytes for L2 Layer. Defaults to 14.
+        """
+
         self.packet = f"{bcol.OKBLUE}[Ethernet]{bcol.ENDC}"
 
-        dest, src, prototype = struct.unpack("!6s6sH", raw_data[:byte_len])
-        dstn_mac = ":".join("%02x" % m for m in dest)
-        src_mac = ":".join("%02x" % s for s in src)
+        destination, source, prototype = struct.unpack("!6s6sH", raw_data[:byte_len])
+        dstn_mac = ":".join("%02x" % m for m in destination)
+        src_mac = ":".join("%02x" % s for s in source)
 
-        eproto = socket.htons(prototype)
+        ethprotocol = socket.htons(prototype)
 
         self.mapping_dict["dstn_mac"] = dstn_mac
         self.mapping_dict["src_mac"] = src_mac
-        self.mapping_dict["etherprotocol"] = eproto
-        if eproto == socket.ntohs(0x0800):
+        self.mapping_dict["etherprotocol"] = ethprotocol
+        if ethprotocol == socket.ntohs(0x0800):
             self.parse_IP_headers(raw_data[14:])
 
-    def parse_L3(self, raw_data):
+    def parse_L3(self, raw_data: str):
+        """Parse L3 Packets
+
+        Args:
+            raw_data (str): Raw packets from the socket
+        """
+
         self.packet += f"{bcol.OKBLUE}[IPv4]{bcol.ENDC}"
 
         iph = unpack("!BBHHHBBH4s4s", raw_data[:20])
@@ -175,7 +211,12 @@ class Firewall:
             self.parse_L3_UDP(raw_data[ihl_len:])
 
     def parseIPv6Head(self, raw_data):
-        self.packet += "\u001b[43;1m[IPv6]\u001b[0m"
+        """Parse IPV4 Packets from raw socket packets
+
+        Args:
+            raw_data (str): Packets from Raw Sockets
+        """
+        self.packet += f"{bcol.OKBLUE}[IPv6]{bcol.ENDC}"
         iph = struct.unpack("!HHHHHH16s16s", raw_data[:20])
 
         traffic_class = iph[5]
@@ -201,7 +242,12 @@ class Firewall:
         elif ipv6protocol == 17:
             self.parse_L3_UDP(raw_data[header_len:])
 
-    def parse_IP_headers(self, raw_data):
+    def parse_IP_headers(self, raw_data: str):
+        """Parsing IP headers based on version
+
+        Args:
+            raw_data (str): Raw Packet Data
+        """
         version_len = raw_data[0]
 
         version = version_len >> 4
@@ -218,15 +264,21 @@ class Firewall:
         self.mapping_dict["icmp4type"] = typ
         self.mapping_dict["icmp4code"] = code
 
-    def parseICMPv6Head(self, raw_data):
-        self.packet += "\u001b[44;1mICMPv6\u001b[0m"
+    def parseICMPv6Head(self, raw_data: str):
+        """Parsing ICMP V6 Packets
+
+        Args:
+            raw_data (str): Packets from Raw Sockets
+        """
+        self.packet += f"{bcol.OKBLUE}ICMPv6{bcol.ENDC}"
         typ, code, _ = struct.unpack("!bbH", raw_data[:4])
 
         self.mapping_dict["icmp6type"] = typ
         self.mapping_dict["icmp6code"] = code
 
     def parse_L3_TCP(self, raw_data):
-        self.packet += "\u001b[46m;1m[TCP]\u001b[0m"
+        """Parsing TCP packets"""
+        self.packet += f"{bcol.OKBLUE}[TCP]{bcol.ENDC}"
         (tcpsrc_port, tcpdest_port, _, _, offset) = struct.unpack("!HHLLH", raw_data[:14])
 
         urg = (offset & 32) >> 5
@@ -245,7 +297,12 @@ class Firewall:
         self.mapping_dict["flag_fin"] = fin
 
     def parse_L3_UDP(self, raw_data):
-        self.packet += "\u001b[47;1m\u001b[30;1m[UDP]\u001b[0m\u001b[0m"
+        """Parse L3 UDP Headers Packets
+
+        Args:
+            raw_data (str): Packets from Raw sockets
+        """
+        self.packet += f"{bcol.OKBLUE}[UDP]{bcol.ENDC}"
         pack = struct.unpack("!4H", raw_data[:8])
 
         self.mapping_dict["udpsrc_port"] = pack[0]
@@ -253,6 +310,7 @@ class Firewall:
         self.mapping_dict["udpdata_len"] = pack[2]
 
     def manageRules(self):
+        """Manage Rules from "rules.json" files"""
         try:
             if os.path.exists("rules.json") == False:
                 rules_template = {"L2": [], "L3v4": [], "L3v6": [], "L4TCP": [], "L4UDP": [], "ICMP": []}
@@ -267,6 +325,14 @@ class Firewall:
             main()
 
     def parse_rules(self, raw_data):
+        """Parse Packets based on rules
+
+        Args:
+            raw_data (str): Raw Packets
+
+        Returns:
+            allow/discard: Allow or discard packets
+        """
 
         start = time.process_time()
         self.parse_L2(raw_data)
@@ -300,6 +366,7 @@ class Firewall:
         return allowed, end
 
     def run(self, internal_MAC, external_MAC):
+        """Runner to take packets and filter based on rules"""
 
         clock = time.time()
 
@@ -307,21 +374,21 @@ class Firewall:
             try:
 
                 all_socks = [self.internal_socket, self.external_socket]
-                ready_socks, _, _ = select.select(all_socks, [], [])
+                interface_sockets, _, _ = select.select(all_socks, [], [])
 
-                for soc in ready_socks:
+                for soc in interface_sockets:
 
                     if time.time() - clock >= 1:
                         self.plot_allow.append(self.allowed)
                         self.plot_discard.append(self.discarded)
                         clock = time.time()
 
-                    raw_data, _ = soc.recvfrom(65565)
+                    # Receive data from the raw socket
+                    raw_data, _ = soc.recvfrom(MEMORY_BUFFER)
 
                     status, ppt = self.parse_rules(raw_data)
 
-                    self.times.append(ppt)
-                    self.mean_time = sum(self.times) / len(self.times)
+                    self.time_stamp.append(ppt)
 
                     if status == True:
                         self.allowed += 1
@@ -344,6 +411,7 @@ class Firewall:
                             f"""{self.packet} \n"""
                             f"""[Src MAC]: {bcol.BOLD}{self.mapping_dict["src_mac"]}{bcol.ENDC}, [Dstn MAC]: {bcol.BOLD}{self.mapping_dict["dstn_mac"]}{bcol.ENDC} \n"""
                             f"""[Src IP]: {bcol.BOLD}{self.mapping_dict["src_ip"]}{bcol.ENDC}, [Dstn IP]: {bcol.BOLD}{self.mapping_dict["dstn_ip"]}{bcol.ENDC} \n"""
+                            f"""[Src Port]: {bcol.BOLD}{self.mapping_dict["tcpsrc_port"]}{bcol.ENDC}, [Dstn Port]: {bcol.BOLD}{self.mapping_dict["tcpdest_port"]}{bcol.ENDC} \n"""
                             f"""[Status]: \u001b[42;1mAllowed\u001b[0m \n"""
                             f"""[PPT] : {round(ppt, 8)}\n\n"""
                         )
@@ -355,6 +423,7 @@ class Firewall:
                             f"""{self.packet}\n"""
                             f"""[Src MAC]: {bcol.BOLD}{self.mapping_dict["src_mac"]}{bcol.ENDC}, [Dstn MAC]: {bcol.BOLD}{self.mapping_dict["dstn_mac"]}{bcol.ENDC} \n"""
                             f"""[SrcIP]: {bcol.BOLD}{self.mapping_dict["src_ip"]}{bcol.ENDC}, [Dstn IP]: {bcol.BOLD}{self.mapping_dict["dstn_ip"]}{bcol.ENDC} \n"""
+                            f"""[Src Port]: {bcol.BOLD}{self.mapping_dict["tcpsrc_port"]}{bcol.ENDC}, [Dstn Port]: {bcol.BOLD}{self.mapping_dict["tcpdest_port"]}{bcol.ENDC} \n"""
                             f"""[Status]: {bcol.FAIL}{"Dropped"}{bcol.ENDC} \n"""
                             f"""[PPT] : {round(ppt, 8)}\n\n"""
                         )
@@ -372,11 +441,15 @@ class Firewall:
                 main()
 
     def analyse_capture(self):
+        """Analyse the packet capture"""
 
+        self.mean_time = sum(self.time_stamp) / len(self.time_stamp)
         print("Firewall Capture Statistics\n")
-        print("Mean Packet Processing Time : ", self.mean_time, "\n")
-        print("No of packets allowed : ", self.allowed, "\n")
-        print("No of packets dropped : ", self.discarded, "\n")
+
+        print(f"No of packets allowed : {self.allowed} \n")
+        print(f"No of packets dropped : {self.discarded} \n")
+        print(f"Mean Packet Processing Time : {round(self.mean_time, 8)} \n")
+
         print(f"No of rules in system : {sum(len(v) for v in self.rules_set.values())}")
 
         plt.title("Firewall Statistics")
@@ -400,12 +473,19 @@ class Firewall:
 
 
 @click.command()
-@click.option("-d", help="DDos Attack Detection", default=0)
+@click.option("-d", help="DDos Attack Detection", default=100)
 @click.option("-s", help="Simple Firewall", default=False, is_flag=True)
-def main(d, s):
+def main(d: int, s: bool):
+    """Main Runner to the Program
+
+    Args:
+        d (int): DDos Threshoold
+        s (bool): Flag to activate Simple Firewall
+    """
     interface1 = "enp1s0"
     interface2 = "enp6s0"
 
+    # Simple Firewall
     if s:
         simple_firewall = SimpleFirewall(interface1, interface2)
         simple_firewall.run()
@@ -417,6 +497,8 @@ def main(d, s):
 
             print("\nStart Firewall with 's', Manage Rules with 'r', Exit with 'e' \n\n")
             key = getkey()
+
+            # Advanced Firewall
             if key == "s":
                 try:
                     firewall.load_rules()
@@ -425,6 +507,7 @@ def main(d, s):
                 clean()
                 firewall.run(internal_MAC="52:54:00:f7:69:35", external_MAC="52:54:00:d6:10:87")
 
+            # Manage Rules
             if key == "r":
                 try:
                     firewall.load_rules()
